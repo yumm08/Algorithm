ff88f56d202c958f88a3b13fdd8b62f704beb0be
yunjiL <91938850+yunjiL@users.noreply.github.com>
2023-12-26 23:22:22 +0900

    [Silver III] Title: 2×n 타일링, Time: 76 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] dp = new int[n + 1];
+
+		dp[0] = 0;
+		dp[1] = 1;
+		if (n >= 2) dp[2] = 2;
+		for (int i = 3; i <= n; i++) {
+			dp[i] = (dp[i - 1] % 10007 + dp[i - 2] % 10007) % 10007;
+		}
+		
+		System.out.println(dp[n]);
+	}
+
+}
