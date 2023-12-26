d281b92d90cb6652d928abb517ae2aa8e09f9d31
yunjiL <91938850+yunjiL@users.noreply.github.com>
2023-12-26 23:18:20 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 596 ms, Memory: 293024 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        StringBuilder prt = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(str.nextToken());
+        int K = Integer.parseInt(str.nextToken());
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            q.add(i);
+        }
+
+        prt.append("<");
+        while (!q.isEmpty()) {
+            
+            for (int i = 0; i < K - 1; i++) {
+                int temp = q.remove();
+                q.add(temp);
+            }
+            prt.append(q.remove());
+            if (!q.isEmpty()) prt.append(", ");
+            else prt.append(">");
+        }
+        System.out.println(prt);
+    }
+    
+}
