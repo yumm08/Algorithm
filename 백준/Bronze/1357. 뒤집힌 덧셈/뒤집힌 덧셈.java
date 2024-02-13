b43f01af820b628e8da70c7b3e467ff2e724d8cb
yunjiL <91938850+yunjiL@users.noreply.github.com>
2024-02-13 23:34:19 +0900

    [Bronze I] Title: 뒤집힌 덧셈, Time: 76 ms, Memory: 11492 KB -BaekjoonHub

---
+++ 백준/Bronze/1357. 뒤집힌 덧셈/뒤집힌 덧셈.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int X = Integer.parseInt(st.nextToken());
+		int Y = Integer.parseInt(st.nextToken());
+
+		System.out.println(rev(rev(X) + rev(Y)));
+	}
+
+	private static int rev(int num) {
+		int result = 0;
+
+		while (num > 0) {
+			result *= 10;
+			result += num % 10;
+			num /= 10;
+		}
+
+		return result;
+	}
+}
