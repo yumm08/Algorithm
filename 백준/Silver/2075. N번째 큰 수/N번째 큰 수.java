f9f12e05bd04d221e2282a910bdc1c587a85ac91
yumm2 <91938850+yumm08@users.noreply.github.com>
2024-09-28 16:40:35 +0900

    Update N번째 큰 수.java

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -1,13 +1,17 @@
 import java.io.*;
 import java.util.*;
 
+/**
+ * PriorityQueue는 heap - BinaryTree 기반
+ * TreeSet은 BinarySearchTree 기반
+ * heap이 이진검색트리 기반의 자료구조들 보다 훨씬 메모리 사용량이 적음
+ */
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
 		int N = Integer.parseInt(br.readLine());
-		int arr[][] = new int[N][N];
 		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 
 		for (int i = 0; i < N; i++) {
